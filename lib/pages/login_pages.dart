97ed5362d8bcccccc435042d9597b7c1565bdf59
Prakash Mishra <prakashmishra5232@gmail.com>
2024-08-10 00:01:57 +0530

    edit login page

--- lib/pages/login_pages.dart
+++ lib/pages/login_pages.dart
@@ -11,7 +11,44 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Login page")),
-    );
+        body: Container(
+      color: Colors.deepPurpleAccent,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 70, horizontal: 30),
+            child: Image.asset(
+              "assets/images/login_image.jpg",
+            ),
+          ),
+         const SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Enter Your E-mail Id",
+                    labelText: "E-mail",
+                  ),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Enter Your Password",
+                    labelText: "Password",
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(onPressed: () {}, child: const Text("Login")),
+              ],
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
